a6b813e98a71d513d2eb425c106b175a975a04a1
sundar <sundar.zsd@gmail.com>
2022-04-11 12:08:15 +0800

    fatal

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+random = [*'a'..'z',*'0'..'9'].sample(8).join
+user = User.create(username: random email: "#{random}@gmail.com", password: random)
