9f0d62674b7613c34815e3c9ff7ae6a8c45f7371
sundar <sundar.zsd@gmail.com>
2022-04-11 12:09:51 +0800

    Revert "fatal"
    This reverts commit a6b813e98a71d513d2eb425c106b175a975a04a1.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-random = [*'a'..'z',*'0'..'9'].sample(8).join
-user = User.create(username: random email: "#{random}@gmail.com", password: random)
